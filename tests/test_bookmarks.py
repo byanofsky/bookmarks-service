1b5755ecd84fb72eeadcc28e16f6b5126de01186
byanofsky <byanofsky@me.com>
2017-08-11 15:02:08 -0700

    Add test for empty bookmarks table

--- tests/test_bookmarks.py
+++ tests/test_bookmarks.py
@@ -21,6 +21,11 @@ class BookmarksTestCase(unittest.TestCase):
         rv = self.app.get('/')
         assert b'Welcome to the bookmarks web service API.' in rv.data
 
+    # Test for no bookmarks
+    def test_no_bookmarks(self):
+        rv = self.app.get('/bookmarks/')
+        assert b'{\n  "bookmarks": []\n}\n' in rv.data
+
 
 if __name__ == '__main__':
     # Make sure we are in testing environment, then switch back
