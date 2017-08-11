0668255e7263ba674463d8fd894f98b6a10ea232
byanofsky <byanofsky@me.com>
2017-08-11 14:13:01 -0700

    Add view with basic front page request handling

---
+++ bookmarks_service/views.py
@@ -0,0 +1,15 @@
+from bookmarks_service import app
+from bookmarks_service.database import db_session
+from bookmarks_service.models import User, Bookmark, Request
+
+# Create user agent for requests
+USER_AGENT = '{}/{}'.format(
+    app.config['USER_AGENT_NAME'],
+    app.config['VERSION_NUMBER'])
+# Timeout for requests library
+TIMEOUT = app.config['TIMEOUT']
+
+
+@app.route('/', methods=['GET'])
+def front_page():
+    return 'Good'
