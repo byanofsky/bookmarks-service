71c8cf05e75906d3306d8b6c4db0d085744c9bfc
byanofsky <byanofsky@me.com>
2017-08-11 13:58:49 -0700

    Adjust py setup files to match application name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bookmarks-service',
-    packages=['bookmarks-service'],
+    packages=['bookmarks_service'],
     version='0.0.1',
     description='A bookmarking and link shortening web service.',
     author='Brandon Yanofsky',
