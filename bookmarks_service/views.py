e12cd356162239c2ab1e76ec9fb280c7f3d3c388
byanofsky <byanofsky@me.com>
2017-08-11 18:16:29 -0700

    Build users route for viewing and adding users

--- bookmarks_service/views.py
+++ bookmarks_service/views.py
@@ -1,5 +1,4 @@
-from flask import jsonify
-from flask import jsonify, request
+from flask import abort, jsonify, make_response, request
 
 from bookmarks_service import app
 from bookmarks_service.database import db_session
@@ -40,3 +39,38 @@ def users():
         users = User.query.all()
         return jsonify(users=users)
     return 'Next steps'
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    if request.method == 'POST':
+        # Get data
+        name = request.form.get('name')
+        email = request.form.get('email')
+        # Verify data sent
+        if not (name and email):
+            return (jsonify(
+                error='Bad Request',
+                code='400',
+                message='Please check request data'
+            ), 400)
+        # Check if user exists with email address
+        if User.query.filter(User.email == email).one_or_none():
+            return (jsonify(
+                error='Conflict',
+                code='409',
+                message='A user with this email already exists'
+            ), 409)
+        # Create user in database
+        u = User(name=name, email=email)
+        db_session.add(u)
+        db_session.commit()
+        # Craft response
+        response = make_response(
+            jsonify(
+                user=u.json()
+            )
+        )
+        response.headers['Location'] = '/users/{}'.format(u.id)
+        return response, 201
+    # Query users and return
+    users = User.query.all()
+    return jsonify(users=[u.json() for u in users])
